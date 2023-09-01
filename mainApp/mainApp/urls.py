f1dd3b9888b2b8c3e6e5542814fb63f9420b2728
Loco <alena.fedorova.12@gmail.com>
2023-09-01 18:02:31 +0200

    добавление файла README.md

--- mainApp/mainApp/urls.py
+++ mainApp/mainApp/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from test.views import TestListCreateView, TestDetailView, QuestionListCreateView, QuestionDetailView, AnswerListCreateView, AnswerDetailView
+from test.views import TestListCreateView, TestDetailView, TestStatsView, QuestionListCreateView, QuestionDetailView, AnswerListCreateView, AnswerDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/test/', TestListCreateView.as_view(), name='test-list'),
     path('api/v1/test/<int:pk>/', TestDetailView.as_view(), name='test-detail'),
-    path('api/v1/question/', QuestionListCreateView.as_view(), name='question-list'),
-    path('api/v1/question/<int:pk>/', QuestionDetailView.as_view(), name='question-detail'),
-    path('api/v1/answer/', AnswerListCreateView.as_view(), name='answer-list'),
-    path('api/v1/answer/<int:pk>/', AnswerDetailView.as_view(), name='answer-detail'),
+    path('api/v1//test/<int:pk>/stats/', TestStatsView.as_view(), name='test-stats'),
+    # path('api/v1/question/', QuestionListCreateView.as_view(), name='question-list'),
+    # path('api/v1/question/<int:pk>/', QuestionDetailView.as_view(), name='question-detail'),
+    # path('api/v1/answer/', AnswerListCreateView.as_view(), name='answer-list'),
+    # path('api/v1/answer/<int:pk>/', AnswerDetailView.as_view(), name='answer-detail'),
 ]
