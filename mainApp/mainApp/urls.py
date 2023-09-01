cf0bd376bb8803415c540c090726eec33e7b3dea
Loco <alena.fedorova.12@gmail.com>
2023-09-01 18:07:10 +0200

    обнровление

--- mainApp/mainApp/urls.py
+++ mainApp/mainApp/urls.py
@@ -7,8 +7,5 @@ urlpatterns = [
     path('api/v1/test/', TestListCreateView.as_view(), name='test-list'),
     path('api/v1/test/<int:pk>/', TestDetailView.as_view(), name='test-detail'),
     path('api/v1//test/<int:pk>/stats/', TestStatsView.as_view(), name='test-stats'),
-    # path('api/v1/question/', QuestionListCreateView.as_view(), name='question-list'),
-    # path('api/v1/question/<int:pk>/', QuestionDetailView.as_view(), name='question-detail'),
-    # path('api/v1/answer/', AnswerListCreateView.as_view(), name='answer-list'),
-    # path('api/v1/answer/<int:pk>/', AnswerDetailView.as_view(), name='answer-detail'),
+
 ]
